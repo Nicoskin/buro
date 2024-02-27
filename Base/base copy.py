9a92ab1479a1cff6e7d75e75495883bad839c4b6
Ivan-PIA <wind.vanya@gmail.com>
2024-02-27 15:30:06 +0700

    add PLL

--- Base/base copy.py
+++ Base/base copy.py
@@ -8,23 +8,40 @@ import mylib.test as mlt
 ###
 bits = ml.str_to_bits('A small text to send to the radio channel') # 328 бита
 sigQpsk = ml.qpsk(bits) 
-#barkerCode = [+1, +1, +1, +1, +1, -1, -1, +1, +1, -1, +1, -1, +1]
-#barkerBpsk = ml.bpsk(barkerCode)
-#barkerBpsk = barkerBpsk * np.exp(1j * np.pi/4) # Доворот на синхры 45 градусов
-#signal = np.concatenate((barkerBpsk, sigQpsk)) # 177 символа (164 без баркера)
+barkerCode = [+1, +1, +1, +1, +1, -1, -1, +1, +1, -1, +1, -1, +1]
+barkerBpsk = ml.bpsk(barkerCode)
+barkerBpsk = barkerBpsk * np.exp(1j * np.pi/4) # Доворот на синхры 45 градусов
+signal = np.concatenate((barkerBpsk, sigQpsk)) # 177 символа (164 без баркера)
 signalRepeat = np.repeat(sigQpsk, 10) # 1770 сэмпла
 
 ###
 ### Работа с SDR
 ###
-sdr = ml.sdr_settings("ip:192.168.3.1", 1900e6, 1000, 1e6, 0, 60) # type: ignore
-ml.tx_sig(sdr,signalRepeat) 
-rx = ml.rx_cycles_buffer(sdr, 40) # берёт 4000 отсчётов
+sdr = ml.sdr_settings("ip:192.168.3.1", 1900e6, 1000, 1e6, 0, 20) # type: ignore
+sdr2 = ml.sdr_settings("ip:192.168.2.1", 1900e6, 1000, 1e6, 0, 20) # type: ignore
+ml.tx_sig(sdr2,signalRepeat) 
+rx = ml.rx_cycles_buffer(sdr, 100) # берёт 4000 отсчётов
 rxMax = max(rx.real)
 rx = rx / rxMax # Нормировка
 
 symbolLength = 10 
 rxConvolve = np.convolve(rx, np.ones(symbolLength)) / 10 # Свёртка
+
+def PLL(conv):
+    mu = 1  
+    theta = 1
+    phase_error = np.zeros(len(conv))  
+    output_signal = np.zeros(len(conv), dtype=np.complex128)
+
+    for n in range(len(conv)):
+        theta_hat = np.angle(conv[n]) 
+        #print(theta_hat)
+        phase_error[n] = theta_hat - theta  
+        output_signal[n] = conv[n] * np.exp(-1j * theta)  
+        theta = theta + mu * phase_error[n]  
+    return output_signal
+
+
 
 def TED_loop_filter(data): #ted loop filter 
     BnTs = 0.01 
@@ -77,11 +94,16 @@ def TED_loop_filter(data): #ted loop filter
     
     return mass1
 
+
+
 Ted_index = TED_loop_filter(rxConvolve)
 print(Ted_index[:10])
 ### тут TED, Loop Filter ...
 ### и мы определяем какой отсчёт брать
 rxАfterTED = rxConvolve[Ted_index]
+rxАfterTED = rxАfterTED[4000:]
+
+rxАfterTED = PLL(rxАfterTED)
 
 maxe = np.argmax(rxConvolve) % 10 # типа TED
 rxSymbols = rxConvolve[maxe::10] # типа TED
@@ -101,13 +123,13 @@ rxSymbols = rxConvolve[maxe::10] # типа TED
 
 ml.cool_plot(rxАfterTED, title="rxАfterTED")
 ml.cool_plot(Ted_index, title="Ted_index")
-ml.cool_scatter(rx, title="Rx signal")
+ml.cool_plot(rx, title="Rx signal")
 # ml.cool_plot(rxConvolve, title="После свёртки")
 # ml.cool_plot(rxClear, title="Повёрнутый")
 
 # ml.cool_scatter(rx, title="Rx signal")
 ml.cool_scatter(rxSymbols, title="Символы")
-ml.cool_scatter(rxАfterTED[1000:2500], title="rxАfterTED")
+ml.cool_scatter(rxАfterTED, title="rxАfterTED")
 # ml.cool_scatter(rxClear, title="Повёрнутый")
 
 
